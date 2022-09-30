45ef05732c35119ee871cdfcdfb1be9dcf275820
Yakshit Poojary <yakshit.p@somaiya.edu>
2022-09-30 14:58:54 +0530

    Update bank.java

--- OOPMT/bank.java
+++ OOPMT/bank.java
@@ -10,6 +10,7 @@ class Customer
         this.name=name;
         this.bal=bal;
     }
+
 }
 
 public class bank 
@@ -19,32 +20,52 @@ public class bank
         Scanner sc = new Scanner(System.in);
 
         String nm;
-        int acc_num,balance,choice;
+        int acc_num,balance,choice,a=0,i;
 
         System.out.print("Enter number of customers: ");
         int n = sc.nextInt();
 
         Customer cust[] = new Customer[100];
 
-        for(int i=0;i<n;i++)
+        for(i=0;i<n;i++)
         {
             Scanner s1 = new Scanner(System.in); 
 
             System.out.println("Enter Name of Customer "+(i+1)+": ");
             nm = s1.nextLine();
-
             System.out.println("Enter Account number of Customer "+(i+1)+": ");
             acc_num = s1.nextInt();
-
-            System.out.println("Enter Name of Customer "+(i+1)+": ");
-            nm = s1.nextLine();
-
             System.out.println("Enter Account Balance of Customer "+(i+1)+": ");
             balance = s1.nextInt();
 
             cust[i]= new Customer(acc_num, nm, balance);
         }
-        
+
+        while(a==0)
+        {
+            System.out.println("1). Add an account\n2). Delete an account\n 3). Display account details\n 4). Exit");
+            choice = sc.nextInt();
+
+            switch(choice)
+            {
+                case 1:
+                    Scanner s1 = new Scanner(System.in); 
+
+                    System.out.println("Enter Name of Customer "+(i+1)+": ");
+                    nm = s1.nextLine();       
+                    System.out.println("Enter Account number of Customer "+(i+1)+": ");
+                    acc_num = s1.nextInt();        
+                    System.out.println("Enter Account Balance of Customer "+(i+1)+": ");
+                    balance = s1.nextInt();
+                    cust[i]= new Customer(acc_num, nm, balance);
+                    i++;
+                    break;
+
+                case 2:
+                    
+            }
+
+        }
     }
 }
 
