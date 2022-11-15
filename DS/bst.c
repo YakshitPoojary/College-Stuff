b9f7e2b4969c20c75b28b13954d087a9781d7c61
Yakshit Poojary <yakshit.p@somaiya.edu>
2022-11-15 14:37:51 +0530

    Create bst.c

---
+++ DS/bst.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node* left;
+    struct node* right;
+};
+
+struct node* insert(struct node* root, int num)
+{
+    struct node* newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->data=num;
+    newnode->left=NULL;
+    newnode->right=NULL;
+    struct node* ptr=root;
+    struct node* prev = NULL;
+    
+    if(root==NULL)
+    {
+        root=newnode;
+    }
+    else
+    {
+        while(ptr != NULL)
+        {
+            prev = ptr;
+            if(newnode -> data <= ptr -> data)
+            {
+                ptr = ptr -> left;
+            }
+            else
+            {
+                ptr = ptr -> right;
+            }
+        }
+        if(newnode -> data <= prev -> data)
+        {
+            prev -> left = newnode;
+        }
+        else
+        {
+            prev -> right = newnode;
+        }
+    }
+    return root;
+}
+
+struct node* delete(struct node* root,int num)
+{
+    
+    struct node* ptr=root;
+    struct node* prev = NULL;
+    
+    if(root==NULL)
+    {
+        printf("There is no root node");
+    }
+    else
+    {
+        while(ptr -> data != num)
+        {
+            prev = ptr;
+            if(num <= ptr-> data)
+            {
+                ptr = ptr -> left;
+            }
+            else
+            {
+                ptr = ptr -> right;
+            }
+        }
+        if(ptr -> left == NULL && ptr -> right != NULL )
+        {
+            if(ptr == prev -> left)
+            {
+                prev -> left = ptr -> right;
+            }
+            else
+            {
+                prev -> right = ptr -> right;
+            }
+            free(ptr);
+        }
+        else if(ptr -> right == NULL && ptr -> left != NULL)
+        {
+            if(ptr == prev -> left)
+            {
+                prev -> left = ptr -> left;
+            }
+            else
+            {
+                prev -> right = ptr -> left;
+            }
+            free(ptr);
+        }
+        else if(ptr -> left == NULL && ptr -> right == NULL)
+        {
+            if(ptr == prev -> left)
+            {
+                prev -> left = NULL;
+            }
+            else
+            {
+                prev -> right =NULL;
+            }
+            free(ptr);
+        }
+    }
+    return root;
+}
+
+void inorder (struct node* ptr)
+{
+    if(ptr)
+    {
+        inorder(ptr->left);
+        printf("%d ",ptr->data);
+        inorder(ptr->right);
+    }
+}
+
+int main() {
+    struct node* root = (struct node*)malloc(sizeof(struct node));
+    root = NULL;
+    int num;
+    root = insert(root,1);
+    root = insert(root,2);
+    root = insert(root,0);
+    root = insert(root,-1);
+    root = insert(root,6);
+    root = insert(root,7);
+    root = insert(root,3);
+    root = insert(root,4);
+    root = delete(root,3);
+    inorder(root);
+    return 0;
+}
