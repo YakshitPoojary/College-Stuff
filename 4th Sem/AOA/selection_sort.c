18c147e0764409f7dcf65b202574f3b2a40b95c3
Yakshit Poojary <yakshit.p@somaiya.edu>
2023-02-07 23:23:53 +0530

    new files

---
+++ 4th Sem/AOA/selection_sort.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int n=5;
+    int inputs[5]={64,25,12,22,11};
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n-i)
+    }
+}
--- 4th Sem/README.md
+++
@@ -1,1 +0,0 @@
-Contains Programs of 4th Sem
