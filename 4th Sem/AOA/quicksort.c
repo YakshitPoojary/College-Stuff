e42cd6a8fd6c02c9f65c71d8af55f4d52d9d87f5
Yakshit Poojary <yakshit.p@somaiya.edu>
2023-02-21 22:45:23 +0530

    Update

---
+++ 4th Sem/AOA/quicksort.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+void interchange(int a[], int i, int j)
+{
+  int temp = a[i];
+  a[i] = a[j];
+  a[j] = temp;
+}
+
+int partition(int a[], int s, int e)
+{
+  int pivot = a[s];
+  int i = s;
+  int j = e;
+
+  do
+  {
+    do
+    {
+      i++;
+    }while(pivot<=a[i]);
+    
+    do
+    {
+      j--;
+    }while(pivot>=a[j]);
+
+    if(i<j)
+    {
+      interchange(a,i,j);
+    }
+  }while(i>=j);
+
+  a[s] = a[j];
+  a[j] = pivot;
+  return j;
+}
+
+void quicksort(int a[], int s, int e)
+{
+  if(s<e)
+  {
+    int pi=partition(a,s,e+1);
+
+    quicksort(a,s,pi-1);
+    quicksort(a,pi+1,e);
+  }
+}
+
+int main()
+{
+  int n=7;
+  int a[7]={25,12,13,5,70,23,10};
+  int s=0,e=n-1;
+
+  quicksort(a,s,e);
+
+}
